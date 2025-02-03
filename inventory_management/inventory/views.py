dd1f54f7eaaee88f81681f75ff565492d382b248
shaunna-h <shaunna.hmcr2022@outlook.com>
2025-02-03 21:59:13 +0000

    created the registration page and sign in

--- inventory_management/inventory/views.py
+++ inventory_management/inventory/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, View
+from django.contrib.auth import authenticate, login, logout
+from .forms import UserRegisterForm
 
 # Basic homepage
 class Index (TemplateView):
     template_name = 'inventory/index.html'
 
+class SignUpView(View):
+    def get(self, request):
+        form = UserRegisterForm()
+        return render(request, 'inventory/signup.html', {'form' : form})
+
+    def post(self, request):
+        form = UserRegisterForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1']
+            )
+
+            login(request, user)
+            return redirect('index')
+        
+        return render(request, 'inventory/signup.html', {'form' : form})
