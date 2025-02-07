f82cafc6b886420be3e50061a2a2f3d53fc39466
shaunna-h <shaunna.hmcr2022@outlook.com>
2025-02-07 22:19:59 +0000

    Create Item

--- inventory_management/inventory/urls.py
+++ inventory_management/inventory/urls.py
@@ -3,12 +3,13 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import Index, SignUpView, user_logout, Dashboard
+from .views import Index, SignUpView, user_logout, Dashboard, AddItem
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('index/', Index.as_view(), name='index'),
     path('dashboard/', Dashboard.as_view(), name='dashboard'),
+    path('add-item/', AddItem.as_view(), name='add-item'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='inventory/login.html'), name='login'),
     path('logout/', user_logout, name='logout'),
