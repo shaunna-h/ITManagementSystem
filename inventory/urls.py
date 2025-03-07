1e8b70c06b1f869f1f0667dfb55505b8df4a1ccf
shaunna-h <shaunna.hmcr2022@outlook.com>
2025-03-07 11:49:37 +0000

    test url fix

--- inventory/urls.py
+++ inventory/urls.py
@@ -7,7 +7,7 @@ from .views import Index, SignUpView, Dashboard, AddItem, EditItem, DeleteItem, 
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', Index.as_view(), name='index'),
+    path('', Index.as_view(template_name='inventory/templates/index.html'), name='index'),
     path('dashboard/', Dashboard.as_view(), name='dashboard'),
     path('add-item/', AddItem.as_view(), name='add-item'),
     path('edit-item/<int:pk>', EditItem.as_view(), name='edit-item'),
